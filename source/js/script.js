4bff9253e6371047af0875d0b99acf5719e7046b
yelog <jaytp@qq.com>
2017-03-04 13:46:21 +0800

    修复提示的tag和author图标显示问题

--- source/js/script.js
+++ source/js/script.js
@@ -72,15 +72,15 @@ function inputChange(e) {
   if(val==""){
     $(".nav-right nav a").css("display","block");
   }else if(val.substr(0,1)=="#"){
+    $("div.ac > ul").attr("class","tag");
     if(val.substr(1).length != 0){
-      $("div.ac > ul").attr("class","tag");
       $("div.acParent").css("display","block");
       $(".nav-right nav a").css("display","none");
       $(".nav-right nav").find("a:contains_tag('"+val.substr(1)+"')").css("display","block");
     }
   }else if (val.substr(0,1)=="@") {
+    $("div.ac > ul").attr("class","author");
     if(val.substr(1).length != 0){
-      $("div.ac > ul").attr("class","author");
       $("div.acParent").css("display","block");
       $(".nav-right nav a").css("display","none");
       $(".nav-right nav").find("a:contains_author('"+val.substr(1)+"')").css("display","block");
