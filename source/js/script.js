868aa4c6ff00becf26eff1524401a2a1d90ad5c6
yelog <jaytp@qq.com>
2017-10-01 12:13:11 +0800

    change: 调整样式

--- source/js/script.js
+++ source/js/script.js
@@ -299,7 +299,7 @@ $(function () {
     }
     if ($(window).width() > 414) {
         /*设置文章列表title宽度*/
-        $('.nav-right>nav>a>.post-title').css('width',$('.nav-right>nav').width() - $('.nav-right>nav>a>.post-date:first').width() - 42)
+        $('.nav-right>nav>a>.post-title').css('width',$('.nav-right>nav>a').width() - $('.nav-right>nav>a>.post-date:first').width() - 40)
     }
 });
 
