2f20844335ac12eda65544f9bde5c28cc46072ef
yelog <jaytp@qq.com>
2017-12-28 21:49:40 +0800

    add: 添加左下角自定义菜单

--- source/js/script.js
+++ source/js/script.js
@@ -18,7 +18,7 @@ jQuery.expr[':'].contains_author = function (a, i, m) {
 /*使用pjax加载页面，速度更快，交互更友好*/
 var content = $(".pjax");
 var container = $(".post");
-$(document).pjax('.nav-right nav a,.nav-left .avatar_target,.nav-left .about', '.pjax', {fragment: '.pjax', timeout: 8000});
+$(document).pjax('.nav-right nav a,.nav-left .avatar_target,.nav-left .site_url', '.pjax', {fragment: '.pjax', timeout: 8000});
 $(document).on({
     /*点击链接后触发的事件*/
     'pjax:click': function () {
